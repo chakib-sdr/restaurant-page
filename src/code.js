5477324bc95736840b3d24cd8aa06972fc62c940
chakib-sdr <chakibsaddar219@gmail.com>
2026-07-28 17:38:54 +0100

    changed the name from img.js to code.js

---
+++ src/code.js
@@ -0,0 +1,49 @@
+import forkIcon from "./restaurant-fork-svgrepo-com.svg";
+import dishIcon from "./pasta-svgrepo-com.svg";
+import dessertIcon from "./cake-svgrepo-com.svg"
+import drinkIcon from "./drinks-party-svgrepo-com.svg";
+import snackIcone from "./pretzel-svgrepo-com.svg";
+
+export const img = document.createElement("img")
+export const buttons = document.querySelectorAll("button")
+export const containersvg = document.querySelector(".containersvg")
+export const menu = document.querySelector("#menu")
+export const about = document.querySelector("#about")
+export const home = document.querySelector(".home")
+export const display = document.querySelector(".display")
+export const information = document.querySelector(".information")
+img.className = "img"
+img.src = forkIcon
+containersvg.appendChild(img)
+
+buttons.forEach(btn => {
+     const  img1 = document.createElement("img")
+     img1.className = "img1"
+    if(btn.textContent ==="Dishes"){
+        img1.src = dishIcon
+        btn.appendChild(img1)
+    }
+    if(btn.textContent === "Dessert"){
+        img1.src = dessertIcon
+        btn.appendChild(img1)
+    }
+    if(btn.textContent === "Drinks"){
+        img1.src = drinkIcon
+        btn.appendChild(img1)
+    }
+    if(btn.textContent === "Snacks"){
+        img1.src = snackIcone
+        btn.appendChild(img1)
+    }
+});
+
+menu.addEventListener("click", () =>{
+    home.style.display = "flex"
+    display.style.display = "block"
+    information.style.display = "none"
+})
+about.addEventListener("click", () =>{
+    home.style.display = "none"
+    display.style.display = "none"
+    information.style.display = "block"
+})
